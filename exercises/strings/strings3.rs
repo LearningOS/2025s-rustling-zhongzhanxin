269d9f1e5bea905c5779c2c497461cbb8464fdb4
zhongzhanxin <3013780685@qq.com>
2025-04-08 21:05:41 +0800

    update

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -6,17 +6,17 @@
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    input.trim();
+    input.trim().to_string()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    input.push(" world!");
+    input.to_owned() + " world!"
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    
+    input.replace("cars", "balloons")
 }
 
 #[cfg(test)]
